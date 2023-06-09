90ae88abb6e3c294cbac95a30fa55f1bf358acad
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-06-10 02:34:02 +0700

    add resource

---
+++ src/script/data/data-source.js
@@ -0,0 +1,20 @@
+import clubs from './clubs.js';
+
+class DataSource {
+  static searchClub(keyword) {
+    return fetch(`https://4r5lltxu.api.sanity.io/v2021-10-21/data/query/production?query=*%5Btitle%20match%20%22${keyword}%22%5D%7B%0A%20%20title%2C%0A%22imgurl%22%3A%20imgurl.asset-%3Eurl%0A%7D%0A%0A`)
+        .then(response => {
+          return response.json();
+        })
+        .then(responseJson => {
+          if (responseJson.result) {
+            console.log(responseJson.result[0].imgurl)
+            return Promise.resolve(responseJson.result);
+          } else {
+            return Promise.reject(`${keyword} is not found`);
+          }
+        });
+  }
+}
+
+export default DataSource;
